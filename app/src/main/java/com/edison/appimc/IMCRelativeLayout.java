cc0d298a857e16799766b22318493cb656258962
edisonmatoso <ejmatoso@gmail.com>
2019-08-26 20:47:55 -0300

    exercicio 1

---
+++ app/src/main/java/com/edison/appimc/IMCRelativeLayout.java
@@ -0,0 +1,13 @@
+package com.edison.appimc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IMCRelativeLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imcrelative_layout);
+    }
+}
