108b50d91aceb943de0c4bae80010ec1a662e3af
edisonmatoso <ejmatoso@gmail.com>
2019-08-26 21:56:12 -0300

    exercicio 2

--- app/src/main/java/com/edison/appimc/IMCRelativeLayout.java
+++ app/src/main/java/com/edison/appimc/IMCRelativeLayout.java
@@ -1,7 +1,12 @@
 package com.edison.appimc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class IMCRelativeLayout extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class IMCRelativeLayout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imcrelative_layout);
+    }
+
+    public void calculaIMC(View view) {
+        EditText edPeso = findViewById(R.id.pesoEdit);
+        EditText edAltura = findViewById(R.id.alturaEdit);
+
+        Double peso = Double.parseDouble(edPeso.getText().toString());
+        Double altura = Double.parseDouble(edAltura.getText().toString());
+
+        TextView imcLabel = findViewById(R.id.imcLabel);
+
+        String calculo = String.valueOf(peso / (altura * altura));
+
+        imcLabel.setText(calculo);
+
+    }
+
+    public void openCredito(View view) {
+        Intent i = new Intent(this, CreditosActivity.class);
+
+        startActivity(i);
     }
 }
